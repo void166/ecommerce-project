49dca46e29b66dc444710f5be057cccbb4074519
void166 <rekihhhrenchin@gmail.com>
2025-08-18 14:47:37 +0800

    6ab has done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/CheckoutPage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Route, Routes } from 'react-router';
 import { OrdersPage } from './pages/OrdersPage';
 
