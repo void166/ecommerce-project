ac57d2b204fd26d8d01dba2651d86083afc80025
void166 <rekihhhrenchin@gmail.com>
2025-08-18 15:13:32 +0800

    6h has done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Route, Routes } from 'react-router';
 import { OrdersPage } from './pages/OrdersPage';
+import { Tracking } from './pages/Tracking';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage />} />
       <Route path='orders' element={<OrdersPage />} />
+      <Route path='tracking' element={<Tracking />} />
     </Routes>
   );
 }
