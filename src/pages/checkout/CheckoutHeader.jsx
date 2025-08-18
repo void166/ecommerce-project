ec95050cb7b85ec0f4b564e93fb79b431c380fcc
void166 <rekihhhrenchin@gmail.com>
2025-08-18 14:48:35 +0800

    6c has done

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -1,21 +1,23 @@
 import './CheckoutHeader.css';
+import { Link } from 'react-router';
+
 export function CheckoutHeader() {
   return (
     <>
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="/">
+            <Link to="/">
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
+            </Link>
           </div>
 
           <div className="checkout-header-middle-section">
             Checkout (
-            <a className="return-to-home-link" href="/">
+            <Link className="return-to-home-link" to="/">
               3 items
-            </a>
+            </Link>
             )
           </div>
 
