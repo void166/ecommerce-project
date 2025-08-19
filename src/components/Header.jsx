680bc131886d11f2dea42f1e5617f42ca4beaa71
void166 <rekihhhrenchin@gmail.com>
2025-08-19 16:37:12 +0800

    get cart from the backend display quntity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { NavLink } from 'react-router';
 import './Header.css';
 
-export function Header() {
+export function Header({ cart }) {
+  let totalQuantity = 0;
+  cart.forEach((cartItem)=>{
+    totalQuantity +=cartItem.quantity;
+  });
   return (
     <>
     
@@ -28,7 +32,7 @@ export function Header() {
 
           <NavLink className="cart-link header-link" to="/checkout">
             <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
+            <div className="cart-quantity">{totalQuantity}</div>
             <div className="cart-text">Cart</div>
           </NavLink>
         </div>
