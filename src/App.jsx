24375e66d1557d5ebeab107cadced724f409858d
void166 <rekihhhrenchin@gmail.com>
2025-08-26 17:12:05 +0800

    got it

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ window.axios =axios;
 function App() {
   const [cart, setCart] = useState([]);
 
-  const loadCart = async()=>{
+  const loadCart = async ()=>{
     const response = await axios.get("/api/cart-items?expand=product");
       setCart(response.data);
   };
