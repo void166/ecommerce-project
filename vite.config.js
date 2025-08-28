acf4328cc701707ff0986b8878b3efdd7a99537a
void166 <rekihhhrenchin@gmail.com>
2025-08-28 14:32:57 +0800

    create new react typescript project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
   server: {
     proxy: {
       '/api': {
